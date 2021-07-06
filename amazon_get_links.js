23280b0e146accda6a2f2e490326518c4b068c07
Kristian Vassilev <kvasilev@devrix.com>
2021-07-06 15:10:07 +0300

    Added function comments

--- amazon_get_links.js
+++ amazon_get_links.js
@@ -1,32 +1,29 @@
 jQuery('input#submit').on('click', (e) => {
     const link_target = document.querySelector('input#amazon_link').value;
     const cache_duration_option = document.querySelector('select#cache_duration').value;
-    
+
+    //Defines the data to work with
     let data = {
         action: 'amazon_get_links',
         amazon_link: link_target, 
         cache_duration_option: cache_duration_option
     }
     if ( data != null ) {
-        
         jQuery.ajax({
             type: "post",
             dataType: "json",
             url: ajax_object.ajax_url,
             data: data,
             success: function (response) {
-                console.log(response.data)
-                //response.data = response.data.replace('<a id="skiplink" tabindex="0" class="skip-link">Skip to main content</a>',"")
+                console.log(response.data);
                 //adds the html form the response.data to the div element
                 jQuery("#my_amazon_div").html(response.data);
-                //console.log(Object.entries(response));
             },
             error: function (response) {
-                console.log(response)  
+                console.log(response);
             }
-        })
-    }
-    
+        });
+    };
 });
 
 
--- onboarding-amazon-link.php
+++ onboarding-amazon-link.php
@@ -10,15 +10,15 @@ if(!defined('ABSPATH')) {
     exit;
 }
 
+//Adds the plugin menu to admin menu
 add_action('admin_menu', 'add_amazon_link_plugin_menu');
 function add_amazon_link_plugin_menu(){
     add_menu_page( 'Amazon link', 'Amazon link', 'administrator', 'amazon-link', 'amazon_link', 'dashicons-amazon' );
 }
 
+//User input and visualization form
 function amazon_link(){
     //start buffering html
-    
-
     ob_start();
     ?>
     <label for="amazon_link"><h2>Amazon link: </h2></label>
@@ -40,19 +40,22 @@ function amazon_link(){
     echo ob_get_clean();
 };
 
-
+//Add the amazon_get_link.js script
 function add_amazon_script() {
     wp_enqueue_script('amazon_get_links', plugins_url('amazon_get_links.js', __FILE__), array('jquery'),false, true);
     wp_localize_script( 'amazon_get_links', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 };
+
 //use admin_enqueue_scripts to enque to the admin only
 add_action( 'admin_enqueue_scripts', 'add_amazon_script' );  
 add_action( 'wp_ajax_amazon_get_links', 'amazon_get_links' );
 
+//Get the ajax request from the user input to the js file and sets the transient data
 function amazon_get_links() {
     //sanitze the url link
     $sanitized_link =  sanitize_url(($_POST['amazon_link']));
 
+    //cache duration logic
     $sanitized_cache_duration = sanitize_text_field(($_POST['cache_duration_option']));
         if ($sanitized_cache_duration =='60') {
             $cache_duration = 60;
@@ -68,8 +71,10 @@ function amazon_get_links() {
     //gets the data from the sanitized link
     $amazon_data = wp_remote_retrieve_body( wp_remote_get( $sanitized_link ) );
     
+    //sets the cache
     $cached_data = set_transient( 'amazon_cached_data', $amazon_data, $cache_duration );
     
+    //sends the data back to js to append it (the html)
     wp_send_json_success($amazon_data);
 }
 ?>
